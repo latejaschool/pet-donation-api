6eeb9c557ac0b423bd6dd34d16d4090826f7964f
Lucas Alves <lucas_alves-07@hotmail.com>
2021-06-11 23:34:35 -0300

    create Entity Address

---
+++ src/Entity/Address.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * @ORM\Entity()
+ */
+class Address
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="uuid")
+     */
+    private Uuid $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private string $street;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private int $number;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private string $district;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private string $complement;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private string $city;
+
+    /**
+     * @ORM\Column(type="string", length=2)
+     */
+    private string $state;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private string $zipcode;
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): void
+    {
+        $this->id = $id;
+    }
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): void
+    {
+        $this->street = $street;
+    }
+
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): void
+    {
+        $this->number = $number;
+    }
+
+    public function getDistrict(): string
+    {
+        return $this->district;
+    }
+
+    public function setDistrict(string $district): void
+    {
+        $this->district = $district;
+    }
+
+    public function getComplement(): string
+    {
+        return $this->complement;
+    }
+
+    public function setComplement(string $complement): void
+    {
+        $this->complement = $complement;
+    }
+
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): void
+    {
+        $this->state = $state;
+    }
+
+    public function getZipcode(): string
+    {
+        return $this->zipcode;
+    }
+
+    public function setZipcode(string $zipcode): void
+    {
+        $this->zipcode = $zipcode;
+    }
+
+}
+
