3a540e2a43462059d31cd5157f6bbb21499da180
Lucas Pamplona <lucas250598@gmail.com>
2021-06-24 20:53:55 -0300

    Isolate NGO delete

--- src/Controller/Api/Breed/DeleteBreedController.php
+++ src/Controller/Api/Breed/DeleteBreedController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller\Api\Breed;
 
 use App\Service\BreedService;
-use Doctrine\Persistence\ObjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
