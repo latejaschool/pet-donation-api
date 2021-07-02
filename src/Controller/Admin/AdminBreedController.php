91fedade179a480b16a4b1a7ef71eab6ed05c7a4
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2021-07-01 21:05:59 -0300

    Add breed on admin page

--- src/Controller/Admin/AdminBreedController.php
+++ src/Controller/Admin/AdminBreedController.php
@@ -4,17 +4,22 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Entity\Breed;
 use App\Service\BreedService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AdminBreedController extends AbstractController
 {
     private BreedService $service;
+    private SerializerInterface $serializer;
 
-    public function __construct(BreedService $service)
+    public function __construct(BreedService $service, SerializerInterface $serializer)
     {
         $this->service = $service;
+        $this->serializer = $serializer;
     }
 
     public function indexAction(): Response
@@ -22,6 +27,23 @@ class AdminBreedController extends AbstractController
         return $this->render('breed/index.html.twig', [
             'breeds' => $this->service->findAll(),
         ]);
+    }
+
+    public function addAction(Request $request): Response
+    {
+        if (!$request->getContent()) {
+            return $this->render('breed/add.html.twig');
+        }
+
+        $breed = $this->serializer->deserialize(
+            json_encode($request->request->all()),
+            Breed::class,
+            'json'
+        );
+
+        $this->service->insert($breed);
+
+        return $this->redirectToRoute('admin_breed_index');
     }
 
     public function removeAction(string $id): Response
